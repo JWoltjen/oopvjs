e3431f3b8797b957caf1ca2f17769bee2b9882ba
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2022-01-03 15:30:58 -0600

    create book class and ui class

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // Book class: Represents a book
 
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title; 
+        this.author = author; 
+        this.isbn = isbn; 
+    }
+
+    
+}
+
 // UI Class: Handle UI tasks
 
 // Store Class: Handles storage
