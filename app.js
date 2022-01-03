f77946bab939da016200904a1f64d81ed58675bb
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2022-01-03 15:44:02 -0600

    display books on load

--- app.js
+++ app.js
@@ -6,16 +6,47 @@ class Book {
         this.author = author; 
         this.isbn = isbn; 
     }
-
-    
 }
 
 // UI Class: Handle UI tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'The Count of Monte Cristo', 
+                author: 'Alexandre Dumas', 
+                isbn: '12345607'
+            }, 
+            {
+                title: 'Candide', 
+                author: 'Voltaire', 
+                isbn: '1525890'
+            }
+        ]; 
+        const books = StoredBooks; 
 
+        books.forEach(book => UI.addBookToList(book)); 
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list'); 
+
+        const row = document.createElement('tr'); 
+
+        row.innerHTML = `
+            <td>${book.title}</td>
+            <td>${book.author}</td>
+            <td>${book.isbn}</td>
+            <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `; 
+
+        list.appendChild(row); 
+    }
+}
 // Store Class: Handles storage
 
 // Event: Display Books
-
+document.addEventListener('DOMContentLoaded', UI.displayBooks); 
 // Event: Add a book
 
 // Event: Remove a Book
